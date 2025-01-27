f7ee0f21739cd9608748e55e2a0094c15a6c0abb
rgarzonj <ruben.garzon@axa.com>
2025-01-27 09:24:32 +0100

    Cleaning before merging

--- tests/test_gpt_structure.py
+++ tests/test_gpt_structure.py
@@ -1,7 +1,8 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+import json
+import re
 import unittest
 from unittest.mock import patch
 from simulation_engine.gpt_structure import gpt_request
@@ -17,11 +18,13 @@ def create_prompt_input(agent_desc, questions):
         str_questions = str_questions.strip()
     return [agent_desc, str_questions]
 
+def clean_json_string(json_string):
+    pattern = r'^```json\s*(.*?)\s*```$'
+    cleaned_string = re.sub(pattern, r'\1', json_string, flags=re.DOTALL)
+    return cleaned_string.strip()
+
 class TestGptRequest(unittest.TestCase):
     def test_gpt_request_success(self):
-        # Mock a successful response
-#        mock_post.return_value.status_code = 200
-#        mock_post.return_value.json.return_value = {'responses': ["Yes", "No", "Sometimes"]}
         agent_desc = "Name is John Doe, 30 year old, very sportive person."
         agent_desc += f"Other observations about the subject:\n\n"
         questions = {
@@ -30,12 +33,13 @@ class TestGptRequest(unittest.TestCase):
         prompt_lib_file = f"{LLM_PROMPT_DIR}/generative_agent/interaction/categorical_resp/singular_v1.txt" 
         prompt_input = create_prompt_input(agent_desc, questions)
         prompt = generate_prompt(prompt_input, prompt_lib_file)
-        print (prompt)
         gpt_version = "gpt-4o-mini"
         response = gpt_request(prompt, gpt_version)
-        print (response)
-        print (response["Response"])
-        self.assertIn("Yes", response["Response"]) or self.assertIn("No", response["Response"]) or self.assertIn("Sometimes", response["Response"])
+        clean_resp = clean_json_string(response)
+        data = json.loads(clean_resp)
+        answer = data["1"]['Response']
+        self.assertIn(answer, ["Yes", "No", "Sometimes"])
+        self.assertEqual(answer, "Yes")
 
 if __name__ == '__main__':
     unittest.main()
